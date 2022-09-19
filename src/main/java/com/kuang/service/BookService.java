cab0ac905b0ce06113071052058613504b15da7a
chenp <641760512@qq.com>
2022-09-19 13:57:22 +0800

    初始化项目：搭建框架 整合mybatis层

---
+++ src/main/java/com/kuang/service/BookService.java
@@ -0,0 +1,22 @@
+package com.kuang.service;
+
+import com.kuang.pojo.Books;
+
+import java.util.List;
+
+public interface BookService {
+    //增加一本书
+    int addBook(Books books);
+
+    //删除一本书
+    int deleteBookById( int id);
+
+    //更新一本书
+    int updateBook(Books books);
+
+    //查询一本书
+    Books queryBookById(int id);
+
+    //查询全部书
+    List<Books> queryAllBook();
+}
---
+++ src/main/java/com/kuang/service/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package com.kuang.service;
+
+import com.kuang.dao.BookMapper;
+import com.kuang.pojo.Books;
+
+import java.util.List;
+
+public class BookServiceImpl implements BookService{
+
+    //service 调dao层，组合dao
+    private BookMapper bookMapper;
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    @Override
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    @Override
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    @Override
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    @Override
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    @Override
+    public List<Books> queryAllBook() {
+        return bookMapper.queryAllBook();
+    }
+}
